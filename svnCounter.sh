2ee6f493b173c7b8a8f78b452557f2f2b07a113d
Clarke Rahrig <crahrig@gmail.com>
2013-10-22 00:47:57 -0500

    Have the diff down to just the insertions and deletions

--- svnCounter.sh
+++ svnCounter.sh
@@ -14,11 +14,11 @@ echo Repository: $1
 # print the repository version
 echo Version: $version
 # print the number of changes between revision
-for i in $(seq 1 $version)
+for i in $(seq 1 $(($version - 1)))
    do
       j=$(($i + 1))
-      changes=svn diff -r $i:$j | diffstat | tail -1
-      echo There are $changes 
+      changes=$(svn diff -r $i:$j | diffstat | tail -1 | sed -e 's/^[^,]*, //')
+      echo There are $changes different lines between R$i and R$j
    done
 # print Done
 echo Done
